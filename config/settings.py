aac5fdda540a9265903cac4a02a745e0beb1be0a
justSevbo <sevbofx@gmail.com>
2022-02-21 18:17:56 +0500

    Templates uchanged

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from blog.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage)
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
