d6e28818c4020de7d3b0fab7dae207eff77975c9
justSevbo <sevbofx@gmail.com>
2022-02-22 19:26:24 +0500

    Custom User added

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import UserChangeForm, UserCreationForm
+
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = CustomUser
+    list_display = ('username', 'email', 'is_staff')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- config/settings.py
+++ config/settings.py
@@ -118,3 +118,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
