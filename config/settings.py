2fe425c85cadcfb4954764b4680012640c659f98
sevbo2003 <sevbofx@gmail.com>
2022-03-09 23:20:37 +0500

    Commit

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'malikdev.herokuapp.com']
 
 # Application definition
 
